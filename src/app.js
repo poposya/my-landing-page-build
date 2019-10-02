f15fbf8b00f7366c54a2a0ffb6274f702b25ebb5
Dmitriy Popov <dmitriy@cleverstaff.net>
2019-10-02 20:01:05 +0300

    stop

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
 import './styles/main.scss';
+import './libs/owl-carousel/dist/owl.carousel.min';
+
+$('.owl-carousel').owlCarousel({
+    animateOut: 'zoomOutLeft',
+    animateIn: 'zoomInRight',
+    items:1,
+    margin:30,
+    loop: false,
+    nav: false,
+    slideTransition: 'ease-in-out',
+    dots: false,
+    stagePadding:30,
+    smartSpeed:450
+});
